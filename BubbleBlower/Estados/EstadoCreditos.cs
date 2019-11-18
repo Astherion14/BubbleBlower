c362c601b638df186cfa94ab1c2bccdb14c80bfd
garrund@gmail.com <garrund@gmail.com>
2019-11-18 09:55:27 +0100

    revert

---
+++ BubbleBlower/Estados/EstadoCreditos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BubbleBlower.Estados
+{
+    public class EstadoCreditos : Estado
+    {
+        public EstadoCreditos(Game1 juego, GraphicsDevice graficos, ContentManager contenido)
+        : base(juego, graficos, contenido)
+        {
+        }
+        public override void Actualizar(GameTime tiempo)
+        {
+        }
+
+        public override void Dibujar(GameTime tiempo, SpriteBatch spriteBatch)
+        {
+        }
+
+        public override void PosActualizado(GameTime tiempo)
+        {
+        }
+    }
+}
---
+++ BubbleBlower/Estados/EstadoRecords.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BubbleBlower.Estados
+{
+    public class EstadoRecords : Estado
+    {
+        public EstadoRecords(Game1 juego, GraphicsDevice graficos, ContentManager contenido)
+        : base(juego, graficos, contenido)
+        {
+
+        }
+        public override void Actualizar(GameTime tiempo)
+        {
+        }
+
+        public override void Dibujar(GameTime tiempo, SpriteBatch spriteBatch)
+        {
+        }
+
+        public override void PosActualizado(GameTime tiempo)
+        {
+        }
+    }
+}
